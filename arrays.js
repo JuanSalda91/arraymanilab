abbe46e3790652b221e736bfd65a49f972f194e2
JuanSalda91 <juansalda9131@gmail.com>
2025-11-07 15:43:07 -0500

    starting task 1

---
+++ arrays.js
@@ -0,0 +1,10 @@
+// -------------------- Task 1 -------------------- //
+
+let shoppingList = []
+
+function addItem(eggs) {
+    shoppingList.push(eggs);
+    console.log(`"${eggs} has been added to your shipping list!"`)
+}
+
+// -------------------- Task 1 -------------------- //
