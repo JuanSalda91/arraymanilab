c3e63fde6125e264bc74990486a08d320d907004
JuanSalda91 <juansalda9131@gmail.com>
2025-11-07 15:59:03 -0500

    task 1 finished

--- arrays.js
+++ arrays.js
@@ -1,10 +1,41 @@
 // -------------------- Task 1 -------------------- //
 
-let shoppingList = []
+let shoppingList = ["milk", "fruits", "vegetables", "ice cream", "butter", "coffee", "sugar", "salt"];
 
-function addItem(eggs) {
-    shoppingList.push(eggs);
-    console.log(`"${eggs} has been added to your shipping list!"`)
+// --- Function to add an item to shoppingList
+function addItem(item) {
+  shoppingList.push(item);
+  console.log(`"${item}" has been added to your shopping list.`);
 }
+
+// --- Function to remove the last item from shoppingList
+function removeLastItem() {
+  if (shoppingList.length === 0) {
+    console.log("The shopping list is already empty.");
+    return;
+  }
+  let removedItem = shoppingList.pop();
+  console.log(`"${removedItem}" has been removed from your shopping list.`);
+}
+
+// --- Function to display all items in shoppingList
+function displayList() {
+  if (shoppingList.length === 0) {
+    console.log("Your shopping list is empty.");
+  } else {
+    console.log("Shopping list items:");
+    shoppingList.forEach((item, index) => {
+      console.log(`${index + 1}. ${item}`);
+    });
+  }
+}
+
+
+displayList();
+addItem("eggs");
+removeLastItem();
+displayList();
+
+
 
 // -------------------- Task 1 -------------------- //
